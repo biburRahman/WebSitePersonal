1e80c071ae1cd9bf1d9f6d62d798a0a49bd3229e
BiburRahman <159100788+biburRahman@users.noreply.github.com>
2024-05-07 22:47:52 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
 menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
+  menu.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
 window.onscroll = () => {
-    navbar.classList.remove('active');
-    menu.classList.remove('bx-x');
-    
+  navbar.classList.remove("active");
+  menu.classList.remove("bx-x");
+};
 
+// kirim pesan
+function Send() {
+  const sendWa = `https://wa.me/6289502826895?text=haii nama saya ${nama.value}, dengan email ${email.value}, saya ingin menyampaikan
+   ${pesan.value}`;
+  window.open(sendWa);
 }
